173afbc0b2d2cc66908019c3bcb9ed2bd264edc1
akjayant <ashish.jayant94@gmail.com>
2022-10-30 22:45:19 +0530

    code cleaning

--- src/mbppo_lagrangian.py
+++ src/mbppo_lagrangian.py
@@ -251,7 +251,9 @@ def ppo(env_fn,cost_limit, actor_critic=core.MLPActorCritic, ac_kwargs=dict(), s
     env = env_fn()
 
     obs_dim = env.observation_space.shape
+    #-----------------This dimension size is specific for our use-case in our modified Safety Gym envs-----------------------
     obs_dim = (26,)
+    #------------------------------------------------------------------------------------------------------------------------
     act_dim = env.action_space.shape
 
     # Create actor-critic module
@@ -367,7 +369,7 @@ def ppo(env_fn,cost_limit, actor_critic=core.MLPActorCritic, ac_kwargs=dict(), s
 
 
     #wandb.watch(ac.pi, log='all', log_freq=10, idx=1)
-    def update(ldecay):
+    def update():
         cur_cost = logger.get_stats('DynaEpCost')[0]
         data = buf.get()
         data['cur_cost'] = cur_cost
@@ -462,11 +464,15 @@ def ppo(env_fn,cost_limit, actor_critic=core.MLPActorCritic, ac_kwargs=dict(), s
     ep_cost = 0
     violations = 0
     # Main loop: collect experience in env to train dynamics models
-    hvio = 0
-    for epoch in range(45):
-        log_std_decay = 1/(1+0.01*epoch)
+    #450k environment steps
+    #------------------Collecting data from real environment----------------------------
+    max_training_steps = int(10000/num_procs())
+    outer_loop_epochs = int(num_steps/max_training_steps)
+    #-----------------------------------------------------------------------------------
+
+    for epoch in range(outer_loop_epochs):
         o, static = env.reset()
-        print(o.shape)
+
         goal_pos = static['goal']
         hazards_pos = static['hazards']
         ld = dist_xy(o[40:],goal_pos)
@@ -478,11 +484,7 @@ def ppo(env_fn,cost_limit, actor_critic=core.MLPActorCritic, ac_kwargs=dict(), s
         print("interacting with real environment")
         mix_real = int(1500/num_procs())
         max_ep_len2 = 80
-        max_training_steps = int(10000/num_procs())
-        #if epoch>30:
-        #    max_training_steps = mix_real
 
-        XC, YC = [],[]
         for t in tqdm(range(max_training_steps)):
 
             #generate hazard lidar
@@ -504,10 +506,7 @@ def ppo(env_fn,cost_limit, actor_critic=core.MLPActorCritic, ac_kwargs=dict(), s
             ep_ret += r
             ep_cost += c
             ep_len += 1
-            #--useless
-            if c>0:
-                hvio+=1
-            #----
+
             #Mixing some real environment samples
             if t<=mix_real-1:
                 buf.store(obs_vec, a, r, c, v, vc, logp)
@@ -551,20 +550,14 @@ def ppo(env_fn,cost_limit, actor_critic=core.MLPActorCritic, ac_kwargs=dict(), s
 
 
 
-        # Perform PPO update!
-        #------------------------train dynamics--------------------------
-        if proc_id()==0:
-            if epoch<=100:
-                train_predict_model(env_pool, predict_env)
-                torch.save(env_model,exp_name+"env_model.pkl")
+        #------------------------Train model dynamics--------------------------
 
-
-
-        comm.barrier()
+        train_predict_model(env_pool, predict_env)
+        torch.save(env_model,exp_name+"env_model.pkl")
         env_name = "safepg2"
         model_type = 'pytorch'
         #predict_env2 = PredictEnv(env_model2, env_name, model_type)
-        #-------------------------
+        #----------------------------------------------------------------------
 
 
 
@@ -587,6 +580,8 @@ def ppo(env_fn,cost_limit, actor_critic=core.MLPActorCritic, ac_kwargs=dict(), s
             dep_cost = 0
             dep_len = 0
             print("training policy with imagination")
+
+            #--------mixing real environment data in first outer loop--------------------------
             if megaiter==0:
                 mix_real = 1500
             else:
@@ -646,7 +641,7 @@ def ppo(env_fn,cost_limit, actor_critic=core.MLPActorCritic, ac_kwargs=dict(), s
                     dep_ret, dep_len, dep_cost = 0, 0, 0
 
 
-            if (epoch % save_freq == 0) or (epoch == epochs-1):
+            if (epoch % save_freq == 0) or (epoch == outer_loop_epochs-1):
                 logger.save_state({'env': env}, None)
             new_dynaret = logger.get_stats('DynaEpRet')[0]
 
@@ -656,7 +651,7 @@ def ppo(env_fn,cost_limit, actor_critic=core.MLPActorCritic, ac_kwargs=dict(), s
                 old_params_pi = get_param_values(ac.pi)
                 old_params_v = get_param_values(ac.v)
                 old_params_vc = get_param_values(ac.vc)
-                update(log_std_decay)
+                update()
                 # 6 ELITE MODELS OUT OF 8
                 valid_rets = [0]*6
                 winner=0
@@ -692,7 +687,7 @@ def ppo(env_fn,cost_limit, actor_critic=core.MLPActorCritic, ac_kwargs=dict(), s
                 if performance_ratio < thresh :
                     perf_flag = False
                     print("backtracking.........")
-                    #------------backtarck-----------------
+                    #------------backtrack-----------------
                     set_param_values(old_params_pi,ac.pi)
                     set_param_values(old_params_v,ac.v)
                     set_param_values(old_params_vc,ac.vc)
@@ -709,11 +704,11 @@ def ppo(env_fn,cost_limit, actor_critic=core.MLPActorCritic, ac_kwargs=dict(), s
                     last_valid_rets = valid_rets
 
             else:
+
+                # Perform PPO-agrangian update!
                 megaiter+=1
-                update(log_std_decay)
+                update()
 
-
-        torch.save(hvio,"hvio_"+exp_name+"_"+str(seed))
         logger.store(Megaiter=megaiter)
         # Log info about epoch
         logger.log_tabular('Epoch', epoch)
@@ -723,8 +718,8 @@ def ppo(env_fn,cost_limit, actor_critic=core.MLPActorCritic, ac_kwargs=dict(), s
         logger.log_tabular('PEPRet', with_min_and_max=True)
         logger.log_tabular('PEPCost', with_min_and_max=True)
         # logger.log_tabular('VVals', with_min_and_max=True)
-
-        logger.log_tabular('TotalEnvInteracts', (epoch+1)*10000)
+        #-------------no. of environment interaction per epoch = max_training_steps = 1e4 --------------------
+        logger.log_tabular('TotalEnvInteracts', (epoch+1)*max_training_steps)
         logger.log_tabular('DynaEpRet', with_min_and_max=True)
         logger.log_tabular('DynaEpCost',with_min_and_max=True)
         logger.log_tabular('Megaiter', with_min_and_max=True)
@@ -749,7 +744,9 @@ if __name__ == '__main__':
     parser.add_argument('--l', type=int, default=2)
     parser.add_argument('--gamma', type=float, default=0.99)
     parser.add_argument('--seed', '-s', type=int, default=0)
+     #---------Don't use multiprocessing as data collection and environment model training is not modified for that-----------#
     parser.add_argument('--cpu', type=int, default=1)
+    #--------------------------------------------------------------------------------------------------------------------------
     parser.add_argument('--steps', type=int, default=4000)
     parser.add_argument('--epochs', type=int, default=50)
     parser.add_argument('--exp_name', type=str, default='ppo')
@@ -769,21 +766,21 @@ if __name__ == '__main__':
     from utils.run_utils import setup_logger_kwargs
     logger_kwargs = setup_logger_kwargs(args.exp_name, args.seed)
     #=================safety gym benchmarks defaults==============================
-    num_steps = 6e5
+    num_steps = 4.5e5
     steps_per_epoch = 30000
     epochs = int(num_steps / steps_per_epoch)
     #=============================================================================
     #---modified safety_gym-------------------------------------------------------
     DEFAULT_ENV_CONFIG_POINT = dict(
         action_repeat=1,
-        max_episode_length=2150,
+        max_episode_length=750,
         use_dist_reward=False,
         stack_obs=False,
     )
     if "Point" in args.env:
         robot = 'Point'
         eplen = 750
-        num_steps = 6e5
+        num_steps = 4.5e5
         steps_per_epoch = 30000
         epochs = 60
         DEFAULT_ENV_CONFIG_POINT['max_episode_length'] = eplen
@@ -791,16 +788,12 @@ if __name__ == '__main__':
         robot = 'Car'
         eplen = 750
         DEFAULT_ENV_CONFIG_POINT['max_episode_length'] = eplen
-        num_steps = 6e5
+        num_steps = 4.5e5
         steps_per_epoch = 30000
         epochs = 60
     env_config=DEFAULT_ENV_CONFIG_POINT
     env = SafetyGymEnv(robot=robot, task="goal", level='2', seed=10, config=env_config)
     state_dim, action_dim = env.observation_size, env.action_size
-
-    #
-    #------ORIGINAL SAFETY GYM------------------------------------------------------
-
 
     if proc_id()==0:
         num_networks = 8
@@ -817,7 +810,6 @@ if __name__ == '__main__':
                                           use_decay=use_decay)
         env_pool = ReplayMemory(replay_size)
         predict_env = PredictEnv(env_model, env_name, model_type)
-
     #-----------------------------------------------------------------------------
 
     ppo(lambda : env, args.cost_limit, actor_critic=core.MLPActorCritic,
